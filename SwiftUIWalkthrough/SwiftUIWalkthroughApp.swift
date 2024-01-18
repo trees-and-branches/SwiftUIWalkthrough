f2e1159a4dd2f31be69c8e5412ee5e95e3412f3d
spoons <ev4n.mus1c@gmail.com>
2024-01-18 07:55:52 -0700

    Initial Commit

---
+++ SwiftUIWalkthrough/SwiftUIWalkthroughApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWalkthroughApp.swift
+//  SwiftUIWalkthrough
+//
+//  Created by shark boy on 1/18/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWalkthroughApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
