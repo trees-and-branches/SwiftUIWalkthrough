f51a3597e5603e71b075811a9d075ef6602a18d1
spoons <ev4n.mus1c@gmail.com>
2024-01-22 10:53:36 -0700

    finished

--- SwiftUIWalkthrough/ContentView.swift
+++ SwiftUIWalkthrough/ContentView.swift
@@ -6,160 +6,79 @@
 //
 
 import SwiftUI
-
-enum Sport: String {
+enum Sport: String, CaseIterable {
+    
     case golf, quidditch, baseketBall, basketWeaving, nothing
     
 }
 
 struct ContentView1: View {
     let buttonSize: CGFloat = 100
-    
-    @State var sport: Sport  = .nothing
+    @State var sports: [Sport] = Sport.allCases
+
     @State var submitToggle = false
+    @State var selectedSport: Sport = .nothing
     
     var body: some View {
         VStack(spacing: 70) {
-        
+            
             Text("Favorite Sport")
                 .font(.largeTitle)
                 .frame(width: 300)
                 .padding()
-        HStack {
             VStack {
-                Button {
-                    sport = .golf
-                } label: {
-                    Text("Golf")
-                        .frame(width: buttonSize, height: buttonSize)
-                     
-                }
-                .tint(sport != .golf ? Color.green.opacity(0.5) : Color.green)
-                Button {
-                    sport = .baseketBall
-                    
-                } label: {
-                    Text("Baseket Ball")
-                        .frame(width: buttonSize, height: buttonSize)
-                        
-                }
-                .tint(sport != .baseketBall ? Color.green.opacity(0.5) : Color.green)
-
-                
-            }
-            
-            VStack {
-                Button {
-                    sport = .quidditch
-                } label: {
-                    Text("Quidditch")
-                        .frame(width: buttonSize, height: buttonSize)
-                        
-                }
-                .tint(sport != .quidditch ? Color.green.opacity(0.5) : Color.green)
-                
-                Button {
-                    sport = .basketWeaving
-                    
-                } label: {
-                    Text("Basket Weaving")
-                        .frame(width: buttonSize, height: buttonSize)
+                HStack {
+                    ForEach($sports[0...1], id: \.rawValue) { $sport in
+                        SportButtonView(sport: $sport) { selectedSport = sport }
+                                    .tint(sport != selectedSport ? Color.green.opacity(0.5) : Color.green)
                         
                     }
-                .tint(sport != .basketWeaving ? Color.green.opacity(0.5) : Color.green)
-                    
-
                 }
-                
-                
+                HStack {
+                    ForEach($sports[2...3], id: \.rawValue) { $sport in
+                        SportButtonView(sport: $sport) { selectedSport = sport }
+                            .tint(sport != selectedSport ? Color.green.opacity(0.5) : Color.green)
+                    }
+                    
+                }
             }
-
-
+            
+            
+            VStack {
+                if submitToggle {
+                    Text("You chose \(selectedSport.rawValue) ")
+                }
+                Button(action: {
+                    submitToggle.toggle()
+                }, label: {
+                    Text("submit")
+                        .frame(width: 200)
+                })
+                .buttonStyle(.borderedProminent)
+            }
             
         }
-//        .tint(Color.green)
-        .buttonStyle(.borderedProminent)
-
-        VStack {
-            if submitToggle {
-                Text("You chose \(sport.rawValue)")
-            }
-            Button(action: {
-                submitToggle.toggle()
-            }, label: {
-                Text("submit")
-                    .frame(width: 200)
-            })
-            .buttonStyle(.borderedProminent)
-        }
-                
+        
     }
-    
 }
 
-
+    struct SportButtonView: View {
+        @Binding var sport: Sport
+        let onTap: () -> Void
+        var body: some View {
+            Button {
+                onTap()
+            } label: {
+                Text(sport.rawValue)
+                    .frame(width: 100, height: 100)
+                
+            }
+            .buttonStyle(.borderedProminent)
+            //            .tint(self.sport !=  ? Color.green.opacity(0.5) : Color.green)
+        }
+    }
 
 
 #Preview {
-        ContentView1()
+ContentView1()
 }
-
-
-//
-//struct ContentView: View {
-//    var body: some View {
-//        VStack {
-//            Text("important text")
-//                .font(.largeTitle)
-//            Spacer()
-//            Text("summer or winter?")
-//            HStack {
-//                Button(action: { },
-//                       label: {
-//                    Text("Summer")
-//                        .padding()
-//                        .frame(width: 150)
-//                }
-//                )
-//                Button(action: { },
-//                       label: {
-//                    Text("Winter")
-//                        .padding()
-//                        .frame(width: 150)
-//                }
-//                )
-//                
-//            }
-//
-//
-//            .foregroundStyle(.white)
-//            .buttonStyle(.borderedProminent) //
-//            
-//            
-//            Text("lightmode or dark mode?")
-//            HStack {
-//                Button(action: { },
-//                       label: {
-//                    Text("Light Mode")
-//                        .padding()
-//                        .frame(width: 150)
-//                }
-//                )
-//                Button(action: { },
-//                       label: {
-//                    Text("Dark Mode")
-//                        .padding()
-//                        .frame(width: 150)
-//                }
-//                )
-//                
-//            }
-//            
-//            .foregroundStyle(.white)
-//            .buttonStyle(.borderedProminent)
-//            
-//            Spacer()
-//        }
-//        .padding()
-//    }
-//}
